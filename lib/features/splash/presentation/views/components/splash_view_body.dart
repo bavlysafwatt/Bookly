7924e57956062cbf9cff8dcb8cfb249b8f59dad7
bavlysafwatt <bavlysafwatt@gmail.com>
2024-09-03 19:38:50 +0300

    navigate to home page

--- lib/features/splash/presentation/views/components/splash_view_body.dart
+++ lib/features/splash/presentation/views/components/splash_view_body.dart
@@ -1,6 +1,9 @@
+import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/assets.dart';
+import 'package:bookly/features/home/presentation/views/home_view.dart';
 import 'package:bookly/features/splash/presentation/views/components/animated_builder.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -17,6 +20,18 @@ class _SplashViewBodyState extends State<SplashViewBody>
   @override
   void initState() {
     super.initState();
+    initAnimationController();
+    navigateToHome();
+  }
+
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.to(() => const HomeView(),
+          transition: Transition.fade, duration: kTransitionDuration);
+    });
+  }
+
+  void initAnimationController() {
     animationController = AnimationController(
       vsync: this,
       duration: const Duration(seconds: 1),
