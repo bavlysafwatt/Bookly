e46fc62615d4f29c63c7513051cd3cb797b2cd93
bavlysafwatt <bavlysafwatt@gmail.com>
2024-09-05 17:38:44 +0300

    implement fetchBestSellerBooks function

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,13 +1,25 @@
 import 'package:bookly/core/errors/failure.dart';
+import 'package:bookly/core/utils/api_service.dart';
 import 'package:bookly/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService = ApiService(Dio());
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() async {
+    try {
+      Map<String, dynamic> data = await apiService.get(
+          endPoint: 'volumes?Filtering=free-ebooks&q=code');
+      List<BookModel> booksList = [];
+      for (var item in data['items']) {
+        booksList.add(BookModel.fromJsonData(item));
+      }
+      return right(booksList);
+    } on Exception catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
