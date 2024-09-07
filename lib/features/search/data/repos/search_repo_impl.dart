e9f6e722b6e3d66806f2e5fe86da3268aa5d84e5
bavlysafwatt <bavlysafwatt@gmail.com>
2024-09-07 15:53:12 +0300

    create search repo

---
+++ lib/features/search/data/repos/search_repo_impl.dart
@@ -0,0 +1,32 @@
+import 'package:bookly/core/errors/failure.dart';
+import 'package:bookly/core/utils/api_service.dart';
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/features/search/data/repos/search_repo.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+class SearchRepoImpl implements SearchRepo {
+  final ApiService apiService;
+
+  SearchRepoImpl(this.apiService);
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchSearchedBooks(
+      {required String key}) async {
+    try {
+      Map<String, dynamic> data = await apiService.get(
+          endPoint: 'volumes?Filtering=free-ebooks&Sorting=relevance&q=$key');
+      List<BookModel> booksList = [];
+      for (var item in data['items']) {
+        booksList.add(BookModel.fromJsonData(item));
+      }
+      return right(booksList);
+    } on Exception catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      } else {
+        return left(ServerFailure(errorMessage: e.toString()));
+      }
+    }
+  }
+}
